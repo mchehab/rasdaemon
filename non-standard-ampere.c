4d44f0a4afa0522849ebcec300b78079cb227a64
Jason Tian <jason@os.amperecomputing.com>
2020-12-23 17:36:00 +0800

    Add one ./configure parameter --enable-amp-na-decode.
    To add code for Ampere specific error, update Makefile.am
    and configure.ac, add non-standard-ampere.c and
    non-standard-ampere.h
    
    Signed-off-by: Jason Tian <jason@os.amperecomputing.com>

---
+++ non-standard-ampere.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020, Ampere Computing LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "ras-record.h"
+#include "ras-logger.h"
+#include "ras-report.h"
+#include "ras-non-standard-handler.h"
+#include "non-standard-ampere.h"
+
