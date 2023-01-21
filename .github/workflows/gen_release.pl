407481c84999826d4d120d999cf2ba8aec84c9ab
Mauro Carvalho Chehab <mchehab@kernel.org>
2023-01-21 14:49:40 +0100

    Add a release workflow
    Should be auto-filling the release information and upload
    a source distro package tarball.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

---
+++ .github/workflows/gen_release.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+my $body_path = shift or die "Need a file name to store the release body";
+
+my $ver;
+
+open IN, "configure.ac" or die;
+while (<IN>) {
+	if (m/^[^\#]*AC_INIT\s*\(\s*\[\s*RASdaemon\s*\]\s*,\s*\[(\d+[\.\d]+)/) {
+		$ver=$1;
+		last;
+	}
+}
+close IN or die;
+
+die if (!$ver);
+
+sub gen_version() {
+	print "$ver\n";
+
+	open IN, "ChangeLog" or return "error opening ChangeLog";
+	open OUT, ">$body_path" or return "error creating $body_path";
+	while (<IN>) {
+		last if (m/$ver/);
+	}
+	while (<IN>) {
+		next if (m/^$/);
+		last if (m/^\S/);
+
+		my $ln = $_;
+		$ln =~ s/^\s+\*/-/;
+		print OUT $ln;
+	}
+	close OUT or return "error closing $body_path";
+
+	return "";
+}
+
+my $ret = gen_version();
+
+die($ret) if ($ret ne "");
