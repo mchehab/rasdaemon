7e6403c041205f0226ab6c76515c23ff43c85063
Ruidong Tian <tianruidong@linux.alibaba.com>
2023-11-23 17:47:25 +0800

    rasdaemon: add mc_event trigger
    Allow users to run a trigger when RAS mc_event occurs, The mc_event
    trigger is separated into CE trigger and UE trigger, this is because
    CE is more frequent than UE, and the CE trigger will lead to more
    performance hits. Users can choose different triggers for CE/UE to
    reduce this effect.
    
    Users can config trigger in /etc/sysconfig/rasdaemon:
    
        TRIGGER_DIR: The trigger diretory
        MC_CE_TRIGGER: The script executed when corrected error occurs.
        MC_UE_TRIGGER: The script executed when uncorrected error occurs.
    
    No script will be executed if MC_CE_TRIGGER/MC_UE_TRIGGER is null.
    
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- ras-events.c
+++ ras-events.c
@@ -44,6 +44,7 @@
 #include "ras-logger.h"
 #include "ras-page-isolation.h"
 #include "ras-cpu-isolation.h"
+#include "trigger.h"
 
 /*
  * Polling time, if read() doesn't block. Currently, trace_pipe_raw never
@@ -60,6 +61,10 @@
 #endif
 
 extern char *choices_disable;
+
+const static struct event_trigger event_triggers[] = {
+	{ "mc_event", &mc_event_trigger_setup },
+};
 
 static int get_debugfs_dir(char *tracing_dir, size_t len)
 {
@@ -274,6 +279,16 @@ int toggle_ras_mc_event(int enable)
 free_ras:
 	free(ras);
 	return rc;
+}
+
+static void setup_event_trigger(char *event)
+{
+	struct event_trigger trigger;
+	for (int i = 0; i < ARRAY_SIZE(event_triggers); i++) {
+		trigger = event_triggers[i];
+		if (!strcmp(event, trigger.name))
+			trigger.setup();
+	}
 }
 
 #ifndef HAVE_BLK_RQ_ERROR
@@ -869,6 +884,8 @@ static int add_event_handler(struct ras_events *ras, struct tep_handle *pevent,
 
 		return EINVAL;
 	}
+
+	setup_event_trigger(event);
 
 	log(ALL, LOG_INFO, "Enabled event %s:%s\n", group, event);
 
---
+++ trigger.c
@@ -0,0 +1,60 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "ras-logger.h"
+#include "trigger.h"
+
+void run_trigger(const char *trigger, char *argv[], char **env, const char* reporter)
+{
+	pid_t child;
+	char *path;
+	int status;
+	char *trigger_dir = getenv("TRIGGER_DIR");
+
+	log(SYSLOG, LOG_INFO, "Running trigger `%s' (reporter: %s)\n", trigger, reporter);
+
+	if (asprintf(&path, "%s/%s", trigger_dir, trigger) < 0)
+		return;
+
+	child = fork();
+	if (child < 0) {
+		log(SYSLOG, LOG_ERR, "Cannot create process for trigger");
+		return;
+	}
+
+	if (child == 0) {
+		execve(path, argv, env);
+		_exit(127);
+	} else {
+		waitpid(child, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status)) {
+			log(SYSLOG, LOG_INFO, "Trigger %s exited with status %d",
+				trigger, WEXITSTATUS(status));
+		} else if (WIFSIGNALED(status)) {
+			log(SYSLOG, LOG_INFO, "Trigger %s killed by signal %d",
+				trigger, WTERMSIG(status));
+		}
+	}
+}
+
+int trigger_check(char *s)
+{
+	char *name;
+	int rc;
+	char *trigger_dir = getenv("TRIGGER_DIR");
+
+	if (trigger_dir) {
+		if (asprintf(&name, "%s/%s", trigger_dir, s) < 0)
+			return -1;
+	} else
+		name = s;
+
+	rc = access(name, R_OK|X_OK);
+
+	if (trigger_dir)
+		free(name);
+
+	return rc;
+}
