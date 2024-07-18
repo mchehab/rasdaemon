56cbb924f3cf46fd0094041eca991cb20d821968
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2024-07-18 18:06:49 +0200

    ras-report.h: avoid long lines
    Better format the stubs on this file to avoid long lines.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- ras-report.h
+++ ras-report.h
@@ -32,41 +32,89 @@
 
 #ifdef HAVE_ABRT_REPORT
 
-int ras_report_mc_event(struct ras_events *ras, struct ras_mc_event *ev);
-int ras_report_aer_event(struct ras_events *ras, struct ras_aer_event *ev);
-int ras_report_mce_event(struct ras_events *ras, struct mce_event *ev);
-int ras_report_non_standard_event(struct ras_events *ras, struct ras_non_standard_event *ev);
-int ras_report_arm_event(struct ras_events *ras, struct ras_arm_event *ev);
-int ras_report_devlink_event(struct ras_events *ras, struct devlink_event *ev);
-int ras_report_diskerror_event(struct ras_events *ras, struct diskerror_event *ev);
-int ras_report_mf_event(struct ras_events *ras, struct ras_mf_event *ev);
-int ras_report_cxl_poison_event(struct ras_events *ras, struct ras_cxl_poison_event *ev);
-int ras_report_cxl_aer_ue_event(struct ras_events *ras, struct ras_cxl_aer_ue_event *ev);
-int ras_report_cxl_aer_ce_event(struct ras_events *ras, struct ras_cxl_aer_ce_event *ev);
-int ras_report_cxl_overflow_event(struct ras_events *ras, struct ras_cxl_overflow_event *ev);
-int ras_report_cxl_generic_event(struct ras_events *ras, struct ras_cxl_generic_event *ev);
-int ras_report_cxl_general_media_event(struct ras_events *ras, struct ras_cxl_general_media_event *ev);
-int ras_report_cxl_dram_event(struct ras_events *ras, struct ras_cxl_dram_event *ev);
-int ras_report_cxl_memory_module_event(struct ras_events *ras, struct ras_cxl_memory_module_event *ev);
+int ras_report_mc_event(struct ras_events *ras,
+			struct ras_mc_event *ev);
+int ras_report_aer_event(struct ras_events *ras,
+			 struct ras_aer_event *ev);
+int ras_report_mce_event(struct ras_events *ras,
+			 struct mce_event *ev);
+int ras_report_non_standard_event(struct ras_events *ras,
+				  struct ras_non_standard_event *ev);
+int ras_report_arm_event(struct ras_events *ras,
+			 struct ras_arm_event *ev);
+int ras_report_devlink_event(struct ras_events *ras,
+			     struct devlink_event *ev);
+int ras_report_diskerror_event(struct ras_events *ras,
+			       struct diskerror_event *ev);
+int ras_report_mf_event(struct ras_events *ras,
+			struct ras_mf_event *ev);
+int ras_report_cxl_poison_event(struct ras_events *ras,
+				struct ras_cxl_poison_event *ev);
+int ras_report_cxl_aer_ue_event(struct ras_events *ras,
+				struct ras_cxl_aer_ue_event *ev);
+int ras_report_cxl_aer_ce_event(struct ras_events *ras,
+				struct ras_cxl_aer_ce_event *ev);
+int ras_report_cxl_overflow_event(struct ras_events *ras,
+				  struct ras_cxl_overflow_event *ev);
+int ras_report_cxl_generic_event(struct ras_events *ras,
+				 struct ras_cxl_generic_event *ev);
+int ras_report_cxl_general_media_event(struct ras_events *ras,
+				       struct ras_cxl_general_media_event *ev);
+int ras_report_cxl_dram_event(struct ras_events *ras,
+			      struct ras_cxl_dram_event *ev);
+int ras_report_cxl_memory_module_event(struct ras_events *ras,
+				       struct ras_cxl_memory_module_event *ev);
 
 #else
 
-static inline int ras_report_mc_event(struct ras_events *ras, struct ras_mc_event *ev) { return 0; };
-static inline int ras_report_aer_event(struct ras_events *ras, struct ras_aer_event *ev) { return 0; };
-static inline int ras_report_mce_event(struct ras_events *ras, struct mce_event *ev) { return 0; };
-static inline int ras_report_non_standard_event(struct ras_events *ras, struct ras_non_standard_event *ev) { return 0; };
-static inline int ras_report_arm_event(struct ras_events *ras, struct ras_arm_event *ev) { return 0; };
-static inline int ras_report_devlink_event(struct ras_events *ras, struct devlink_event *ev) { return 0; };
-static inline int ras_report_diskerror_event(struct ras_events *ras, struct diskerror_event *ev) { return 0; };
-static inline int ras_report_mf_event(struct ras_events *ras, struct ras_mf_event *ev) { return 0; };
-static inline int ras_report_cxl_poison_event(struct ras_events *ras, struct ras_cxl_poison_event *ev) { return 0; };
-static inline int ras_report_cxl_aer_ue_event(struct ras_events *ras, struct ras_cxl_aer_ue_event *ev) { return 0; };
-static inline int ras_report_cxl_aer_ce_event(struct ras_events *ras, struct ras_cxl_aer_ce_event *ev) { return 0; };
-static inline int ras_report_cxl_overflow_event(struct ras_events *ras, struct ras_cxl_overflow_event *ev) { return 0; };
-static inline int ras_report_cxl_generic_event(struct ras_events *ras, struct ras_cxl_generic_event *ev) { return 0; };
-static inline int ras_report_cxl_general_media_event(struct ras_events *ras, struct ras_cxl_general_media_event *ev) { return 0; };
-static inline int ras_report_cxl_dram_event(struct ras_events *ras, struct ras_cxl_dram_event *ev) { return 0; };
-static inline int ras_report_cxl_memory_module_event(struct ras_events *ras, struct ras_cxl_memory_module_event *ev) { return 0; };
+static inline int ras_report_mc_event(struct ras_events *ras,
+				      struct ras_mc_event *ev)
+{ return 0; };
+static inline int ras_report_aer_event(struct ras_events *ras,
+				       struct ras_aer_event *ev)
+{ return 0; };
+static inline int ras_report_mce_event(struct ras_events *ras,
+				       struct mce_event *ev)
+{ return 0; };
+static inline int ras_report_non_standard_event(struct ras_events *ras,
+						struct ras_non_standard_event *ev)
+{ return 0; };
+static inline int ras_report_arm_event(struct ras_events *ras,
+				       struct ras_arm_event *ev)
+{ return 0; };
+static inline int ras_report_devlink_event(struct ras_events *ras,
+					   struct devlink_event *ev)
+{ return 0; };
+static inline int ras_report_diskerror_event(struct ras_events *ras,
+					     struct diskerror_event *ev)
+{ return 0; };
+static inline int ras_report_mf_event(struct ras_events *ras,
+				      struct ras_mf_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_poison_event(struct ras_events *ras,
+					      struct ras_cxl_poison_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_aer_ue_event(struct ras_events *ras,
+					      struct ras_cxl_aer_ue_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_aer_ce_event(struct ras_events *ras,
+					      struct ras_cxl_aer_ce_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_overflow_event(struct ras_events *ras,
+						struct ras_cxl_overflow_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_generic_event(struct ras_events *ras,
+					       struct ras_cxl_generic_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_general_media_event(struct ras_events *ras,
+						     struct ras_cxl_general_media_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_dram_event(struct ras_events *ras,
+					    struct ras_cxl_dram_event *ev)
+{ return 0; };
+static inline int ras_report_cxl_memory_module_event(struct ras_events *ras,
+						     struct ras_cxl_memory_module_event *ev)
+{ return 0; };
 
 #endif
 
