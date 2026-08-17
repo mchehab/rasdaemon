72d14a45a6f764fd52b348b365fc5522b9a52404
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2026-08-17 14:59:04 +0200

    actions: don't require newer versions of cmocka
    It sounds that cmocka only added output filtering on version 2.x.
    We don't need that for CI.
    
    So, add a backward-compatible code.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- tests/unittest.h
+++ tests/unittest.h
@@ -3,9 +3,16 @@
  * Copyright (C) 2026 Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
  */
 
+#include <setjmp.h>
+
 #include <cmocka.h>
 
 #include <core/types.h>
+
+#ifndef CMOCKA_VERSION_2
+#define assert_non_null_msg(x, y) assert_non_null(x)
+#define assert_null_msg(x, y) assert_null(x)
+#endif
 
 #ifndef TEST_GROUPS_H
 #define TEST_GROUPS_H
